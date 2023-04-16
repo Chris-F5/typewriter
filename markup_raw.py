1f629b3564c025ce4a08e10c80c496dcb8eb9b57
Christopher Lang <christopher.lang.256@gmail.com>
2023-04-16 20:24:00 +0100

    Add orphan and widow control to markup_raw

--- markup_raw.py
+++ markup_raw.py
@@ -5,16 +5,26 @@ import sys, argparse
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("-s", "--font_size", type=int, default=12)
 arg_parser.add_argument("-f", "--font_name", default="Monospace")
+arg_parser.add_argument("-o", "--orphans", type=int, default=1)
+arg_parser.add_argument("-w", "--widows", type=int, default=1)
 args = arg_parser.parse_args()
 
 font_size = args.font_size
 font = args.font_name
+orphans = args.orphans
+widows = args.widows
 
-for line in sys.stdin:
+lines = sys.stdin.readlines()
+
+i = 0
+for line in lines:
+  i += 1
   line = line.replace('\n', '')
   print("box {}".format(font_size))
   print("START TEXT")
   print("FONT {} {}".format(font, font_size))
   print('STRING "{}"'.format(line.replace('\\', '\\\\').replace('"', '\\"')))
   print("END")
-  print("opt_break")
+  if i >= orphans and len(lines) - i >= widows:
+    print("opt_break")
+print("opt_break")
