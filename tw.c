95e9289495405689a5695f2ade0ae8b22a13e009
Christopher Lang <christopher.lang.256@gmail.com>
2023-04-28 10:41:46 +0100

    Comment tw.c more

--- tw.c
+++ tw.c
@@ -3,6 +3,12 @@
  * See LICENSE for license details.
  */
 
+/*
+ * tw.c
+ * tw is short for typewriter. This file reads _pages_ from stdin and converts
+ * this to PDF.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -43,12 +49,13 @@ add_page(FILE *pdf_file, int obj_parent, struct pdf_xref_table *xref,
 {
   int obj_content, obj_page;
   long length;
+  /* Allocate pdf indirecto objects for the page and its conetnt. */
   obj_content = allocate_pdf_obj(xref);
   obj_page = allocate_pdf_obj(xref);
 
   /* ET is the PDF control sequence for ending text content. */
   dbuffer_printf(text_content, "ET\n");
-  /* Write a PDF stream object containing the page content. */
+  /* Write a PDF stream indirect object containing the page content. */
   length = text_content->size + graphic_content->size;
   pdf_start_indirect_obj(pdf_file, xref, obj_content);
   fprintf(pdf_file, "<< /Length %ld >> stream\n", length);
@@ -73,9 +80,11 @@ add_page(FILE *pdf_file, int obj_parent, struct pdf_xref_table *xref,
 static void
 write_pdf_escaped_string(struct dbuffer *buffer, const char *string)
 {
+  /* PDF string is enclosed in brackets. */
   dbuffer_putc(buffer, '(');
   while (*string) {
     switch (*string) {
+      /* Brackets and backshalses need to be escaped. */
       case '(':
       case ')':
       case '\\':
@@ -130,8 +139,10 @@ parse_text(FILE *input, int x, int y, struct text_content *text_content,
         fprintf(stderr, "Text FONT command's 2nd argument must be integer.\n");
         continue;
       }
+      /* Update the current font name and size. */
       strcpy(font_name, record.fields[1]);
       font_size = arg1;
+      /* Tell _resources_ that this font is being used. */
       include_font_resource(resources, font_name);
       continue;
     }
@@ -145,6 +156,7 @@ parse_text(FILE *input, int x, int y, struct text_content *text_content,
         fprintf(stderr, "Text SPACE command's argument must be integer.\n");
         continue;
       }
+      /* Update current word spacing. */
       word_spacing = arg1;
       continue;
     }
@@ -154,27 +166,37 @@ parse_text(FILE *input, int x, int y, struct text_content *text_content,
         fprintf(stderr, "Text STRING command must take 1 argument.\n");
         continue;
       }
+      /* If the font has not been set. */
       if (*font_name == '\0') {
         fprintf(stderr, "Text must specify font.\n");
         continue;
       }
+      /* If a different font is used for this string. */
       if (strcmp(text_content->font_name, font_name)
           || text_content->font_size != font_size) {
+        /* Change the active font. */
         dbuffer_printf(&text_content->buffer, " /%s %d Tf", font_name,
             font_size);
         strcpy(text_content->font_name, font_name);
         text_content->font_size = font_size;
       }
+      /* If a different word spacing is used for this string. */
       if (word_spacing != text_content->word_spacing) {
+        /*
+         * Change the current word spacing. Divide by 1000 to convert to point.
+         */
         dbuffer_printf(&text_content->buffer, " %f Tw",
             (float)word_spacing / 1000);
         text_content->word_spacing = word_spacing;
       }
+      /* Separate this text command from the last. */
       dbuffer_putc(&text_content->buffer, ' ');
+      /* Write the string command. */
       write_pdf_escaped_string(&text_content->buffer, record.fields[1]);
       dbuffer_printf(&text_content->buffer, " Tj");
-      continue;
+      continue; /* Next record. */
     }
+    /* If the first field was not recognised. */
     fprintf(stderr, "Invalid text command: '%s'\n", record.fields[0]);
   }
   dbuffer_putc(&text_content->buffer, '\n');
@@ -207,11 +229,13 @@ parse_graphic(FILE *input, int origin_x, int origin_y,
         fprintf(stderr, "Graphic MOVE command must take 2 arguments.\n");
         continue;
       }
+      /* Try to convert the offset arguments to integers. */
       if (str_to_int(record.fields[1], &arg1)
           || str_to_int(record.fields[2], &arg2)) {
         fprintf(stderr, "Graphic MOVE command takes only integer arguments.\n");
         continue;
       }
+      /* Update the current position. */
       x = origin_x + arg1;
       y = origin_y + arg2;
       continue;
@@ -234,9 +258,10 @@ parse_graphic(FILE *input, int origin_x, int origin_y,
           return 1;
         continue;
       }
+      /* The first argument was not recognised. */
       fprintf(stderr, "Invalid graphic START command argument: '%s'\n",
           record.fields[1]);
-      return 1;
+      return 1; /* Error. */
     }
     if (strcmp(record.fields[0], "IMAGE") == 0) {
       /* IMAGE [WIDTH] [HEIGHT] [IMAGE_FILE_NAME] */
@@ -244,12 +269,14 @@ parse_graphic(FILE *input, int origin_x, int origin_y,
         fprintf(stderr, "IMAGE command must take 3 arguments.\n");
         continue;
       }
+      /* Convert width and height to integers. */
       if (str_to_int(record.fields[1], &arg1)
           || str_to_int(record.fields[2], &arg2)) {
         fprintf(stderr,
             "IMAGE command's 1st and 2nd arguments must be integer.\n");
         continue;
       }
+      /* Tell _resources_ that we are using this image. */
       image_id = include_image_resource(resources, record.fields[3]);
       /* Push graphic state. */
       dbuffer_printf(graphic_content, "q\n");
@@ -278,12 +305,15 @@ print_pages(FILE *pages_file, FILE *typeface_file, FILE *pdf_file)
   struct text_content text_content;
   struct dbuffer graphic_content;
   ret = 0;
+  /* Initialise resources. */
   init_pdf_xref_table(&xref_table);
   init_pdf_page_list(&page_list);
   init_pdf_resources(&resources);
+  /* Allocate essential indirect objects. */
   obj_resources = allocate_pdf_obj(&xref_table);
   obj_page_list = allocate_pdf_obj(&xref_table);
   obj_catalog = allocate_pdf_obj(&xref_table);
+  /* Write PDF header. */
   pdf_write_header(pdf_file);
 
   text_content.x = 0;
@@ -291,30 +321,35 @@ print_pages(FILE *pages_file, FILE *typeface_file, FILE *pdf_file)
   text_content.font_size = 0;
   text_content.font_name[0] = '\0';
   dbuffer_init(&text_content.buffer, 1024 * 32, 1024 * 32);
+  /* Begin Text. */
   dbuffer_printf(&text_content.buffer, "BT\n");
   dbuffer_init(&graphic_content, 1024 * 4, 1024 * 4);
   init_record(&record);
-  /* Parse document mode _pages_. */
+  /* Parse each record in _pages_ document mode. */
   for (;;) {
     parse_result = parse_record(pages_file, &record);
-    if (parse_result == EOF)
+    if (parse_result == EOF) /* Stop at end of file. */
       break;
-    if (parse_result)
+    if (parse_result) /* If failed to parse record then skip it. */
       continue;
     if (strcmp(record.fields[0], "START") == 0) {
       if (record.field_count != 2) {
         fprintf(stderr, "Pages START command must take 1 argument.\n");
+        /* ret=1 because this is an error and we will return 1. */
         ret = 1;
         break;
       }
       if (strcmp(record.fields[1], "PAGE") == 0) {
+        /* Try to parse this PAGE's graphic. */
         if (parse_graphic(pages_file, 0, 0, &text_content, &graphic_content,
               &resources)) {
           ret = 1;
           break;
         }
+        /* Add the page to the pdf file. */
         add_page(pdf_file, obj_page_list, &xref_table, &page_list,
             &text_content.buffer, &graphic_content);
+        /* Reset the text and graphic for the next page. */
         text_content.x = 0;
         text_content.y = 0;
         text_content.font_size = 0;
@@ -331,9 +366,11 @@ print_pages(FILE *pages_file, FILE *typeface_file, FILE *pdf_file)
       break;
     }
   }
+  /* Cleanup resources. */
   dbuffer_free(&text_content.buffer);
   free_record(&record);
 
+  /* Add resources to PDF file and close it. */
   pdf_add_resources(pdf_file, typeface_file, obj_resources, &resources,
       &xref_table);
 
@@ -359,7 +396,9 @@ main(int argc, char **argv)
   int opt;
   const char *output_file_name;
   FILE *pages_file, *typeface_file, *pdf_file;
+  /* Default output to ./output.pdf */
   output_file_name = "./output.pdf";
+  /* Parse command line arguments. */
   while ( (opt = getopt(argc, argv, "o:")) != -1) {
     switch (opt) {
       case 'o':
@@ -371,6 +410,12 @@ main(int argc, char **argv)
     }
   }
   pages_file = stdin;
+  /*
+   * Typeface file is hardcoded to be called 'typeface'. This is a design
+   * decision: if we could call the typeface file anything we wanted, then
+   * other programs part of the typesetting process would struggle to locate
+   * it. Just like 'Makefile', 'typeface' is a hard-coded file name.
+   */
   typeface_file = fopen("./typeface", "r");
   pdf_file = fopen(output_file_name, "w");
   if (typeface_file == NULL) {
