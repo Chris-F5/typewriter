3daebd5c749294aa7c1bc5bdd1785ad95c940092
Christopher Lang <christopher.lang.256@gmail.com>
2023-10-22 14:34:45 +0100

    Remove unused code

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "tw.h"
+#include "utils.h"
 
 void *
 xmalloc(size_t len)
@@ -23,32 +23,4 @@ xrealloc(void *p, size_t len)
   if ( (p = realloc(p, len)) == NULL)
     perror("realloc");
   return p;
-}
-
-int
-is_font_name_valid(const char *font_name)
-{
-  unsigned char c;
-  if (*font_name == '\0')
-    return 1;
-  while (*font_name) {
-    c = *font_name;
-    if (c == 45 || (c >= 48 && c <= 57) || c == 95 || (c >= 65 && c <= 90)
-        || (c >= 97 && c <= 122)) {
-      font_name++;
-    } else {
-      return 0;
-    }
-  }
-  return 1;
-}
-
-int
-str_to_int(const char *str, int *n)
-{
-  char *endptr;
-  *n = strtol(str, &endptr, 10);
-  if (*str == '\0' || *endptr != '\0')
-    return 1;
-  return 0;
 }
